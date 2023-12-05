5c40c69fd7228f726194a9a9d3cbc0399ac253f6
STimour <st.sayfoutdinov@icloud.com>
2023-12-05 09:34:30 +0100

    ajout de cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
+import cors from 'cors'
 dotenv.config();
 
 const APP: Express = express();
 const PORT = process.env.PORT;
+
+APP.use(cors({
+  origin: '*', //accepte all
+  methode: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}))
 
 APP.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
